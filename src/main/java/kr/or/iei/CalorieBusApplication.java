8a5282433efe004c0ed6224861077c9c1a671698
makhai92 <makkai9210@gmail.com>
2024-08-02 09:39:22 +0900

    Initial commit

---
+++ src/main/java/kr/or/iei/CalorieBusApplication.java
@@ -0,0 +1,13 @@
+package kr.or.iei;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalorieBusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CalorieBusApplication.class, args);
+	}
+
+}
