379bca06927aef04182311cefccd93e84ec9b7d0
dhk119 <skysiwoo5@naver.com>
2024-08-02 17:18:01 +0900

    자유게시판 버전1

--- src/main/java/kr/co/caloriebus/board/controller/BoardController.java
+++ src/main/java/kr/co/caloriebus/board/controller/BoardController.java
@@ -3,6 +3,7 @@ package kr.co.caloriebus.board.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import kr.co.caloriebus.board.model.service.BoardService;
@@ -20,4 +21,11 @@ public class BoardController {
 	
 	@Autowired
 	private FileUtils fileUtils;
+	
+	@RequestMapping(value="/list")
+	public String list(String category,int reqPage,Model model) {
+		
+		model.addAttribute("category",category);
+		return "board/list";
+	}
 }
