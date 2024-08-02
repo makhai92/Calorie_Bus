ad962d8f1cb9af0268d1b6d3a09e4ead61e55fe5
makhai92 <makkai9210@gmail.com>
2024-08-02 17:19:54 +0900

    헤더푸터(ProtoType3)

--- src/main/java/kr/co/caloriebus/member/controller/MemberController.java
+++ src/main/java/kr/co/caloriebus/member/controller/MemberController.java
@@ -2,6 +2,7 @@ package kr.co.caloriebus.member.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import kr.co.caloriebus.member.model.service.MemberService;
@@ -11,4 +12,14 @@ import kr.co.caloriebus.member.model.service.MemberService;
 public class MemberController {
 	@Autowired
 	private MemberService memberService;
+	
+	@GetMapping(value="/join")
+	public String join() {
+		return "member/join";
+	}
+	
+	@GetMapping(value="/joinForm")
+	public String joinForm() {
+		return "member/joinForm";
+	}
 }
--- src/main/java/kr/co/caloriebus/product/controller/ProductController.java
+++ src/main/java/kr/co/caloriebus/product/controller/ProductController.java
@@ -3,6 +3,7 @@ package kr.co.caloriebus.product.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import kr.co.caloriebus.product.model.service.ProductService;
@@ -20,4 +21,9 @@ public class ProductController {
 	
 	@Autowired
 	private FileUtils fileUtils;
+	
+	@GetMapping(value="/list")
+	public String list() {
+		return "/product/list";
+	}
 }
