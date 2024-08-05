b42489ed3e7aab12d3b19a972bf8ed4b83aa1e82
seong3449 <tpdud3449@naver.com>
2024-08-05 18:35:48 +0900

    write.html 커밋

--- src/main/java/kr/co/caloriebus/product/controller/ProductController.java
+++ src/main/java/kr/co/caloriebus/product/controller/ProductController.java
@@ -6,13 +6,13 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.multipart.MultipartFile;
 
 import kr.co.caloriebus.product.model.dto.Product;
-import kr.co.caloriebus.product.model.dto.ProductFile;
 import kr.co.caloriebus.product.model.service.ProductService;
 import kr.co.caloriebus.util.FileUtils;
 
@@ -30,7 +30,9 @@ public class ProductController {
 	private FileUtils fileUtils;
 	
 	@GetMapping(value="/list")
-	public String list() {
+	public String list(Model model) {
+		List list = productService.selectAllProduct();
+		model.addAttribute("list",list);
 		return "/product/list";
 	}
 	
@@ -40,16 +42,31 @@ public class ProductController {
 	}
 	
 	@PostMapping(value="/insert")
-	public String insert(Product p, MultipartFile[] upfile) {
+	public String insert(Product p, MultipartFile upfile,Model model) {
+		String savepath = root+"/product/main/";
+		String filepath = fileUtils.upload(savepath, upfile);
+		p.setProductImg(filepath);
 		int result = productService.productInsert(p);
 		if(result>0) {
+			model.addAttribute("title","작성완료");
+			model.addAttribute("msg","게시글이 작성되었습니다.");
+			model.addAttribute("icon","success");
+		}else {
+			model.addAttribute("title","작성실패");
+			model.addAttribute("msg","문제가 발생하였습니다.");
+			model.addAttribute("icon","error");
+		}
+		model.addAttribute("loc","/product/list");
+		return "common/msg";
+		/*
+		if(result>0) {
 			//productNo를 조회해와서 productFile에 이미지 추가 등록
-			Product product = productService.selectProductNo(p.getProductTitle());
+			Product product = productService.selectOneProduct(p.getProductTitle());
 			
 			List<ProductFile> fileList = new ArrayList<ProductFile>();
 			if (!upfile[0].isEmpty()) {
 				// C:/Temp/upload/product/main
-				String savepath = root + "/product/main";
+				String savepath = root + "/product/main/";
 				for (MultipartFile file : upfile) {
 					// 사용자가 업로드한 파일 이름 출력
 					String filename = file.getOriginalFilename(); 
@@ -63,9 +80,13 @@ public class ProductController {
 			}
 			int result2 = productService.productFileInsert(fileList);
 			if(result2>0) {
-				
+				model.addAttribute("title", "작성성공!");
+				model.addAttribute("msg", "공지사항 작성에 성공했습니다.");
+				model.addAttribute("icon", "success");
+				model.addAttribute("loc", "/product/list");
+				return "common/msg";
 			}
 		}
-		return "/product/list";
+		*/
 	}
 }
