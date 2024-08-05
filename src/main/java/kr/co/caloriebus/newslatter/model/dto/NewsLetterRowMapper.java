6bd22bf664c94810b81bb71fc5104e8a1104b030
makhai92 <makkai9210@gmail.com>
2024-08-05 13:33:26 +0900

    프론트3

--- src/main/java/kr/co/caloriebus/newslatter/model/dto/NewsLetterRowMapper.java
+++ src/main/java/kr/co/caloriebus/newslatter/model/dto/NewsLetterRowMapper.java
@@ -1,5 +1,14 @@
 package kr.co.caloriebus.newslatter.model.dto;
 
-public class NewsLetterRowMapper {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+import kr.co.caloriebus.newsletter.model.service.NewsLetterService;
+
+@Controller
+@RequestMapping(value = "/newsletter")
+public class NewsLetterRowMapper {
+	@Autowired
+	private NewsLetterService newsLetterService;
 }
