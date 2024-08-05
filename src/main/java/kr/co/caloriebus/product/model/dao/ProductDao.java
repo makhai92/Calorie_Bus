d4ff7c9e0933386b14697d6c92d8579569cdb2f9
seong3449 <tpdud3449@naver.com>
2024-08-05 16:38:17 +0900

    writeFrm 커밋

--- src/main/java/kr/co/caloriebus/product/model/dao/ProductDao.java
+++ src/main/java/kr/co/caloriebus/product/model/dao/ProductDao.java
@@ -1,9 +1,13 @@
 package kr.co.caloriebus.product.model.dao;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
+import kr.co.caloriebus.product.model.dto.Product;
+import kr.co.caloriebus.product.model.dto.ProductFile;
 import kr.co.caloriebus.product.model.dto.ProductFileRowMapper;
 import kr.co.caloriebus.product.model.dto.ProductRowMapper;
 
@@ -15,4 +19,32 @@ public class ProductDao {
 	private ProductRowMapper productRowMapper;
 	@Autowired
 	private ProductFileRowMapper productFileRowMapper;
+	
+	
+	public int productInsert(Product p) {
+		String query = "insert into product values(product_seq.nextval,?,?,?,?,?,?,?,?)";
+		Object[] params = {p.getProductTitle(),p.getProductContent(),p.getProductPrice(),p.getProductDcPrice(),p.getProductMinCount(),p.getProductMaxCount(),p.getStartDate(),p.getEndDate()};
+		int result = jdbc.update(query, params);
+		return result;
+	}
+
+
+	public Product selectProductNo(String productTitle) {
+		String query = "select * from product where product_title = ?";
+		Object[] params = {productTitle};
+		List list = jdbc.query(query, productFileRowMapper, params);
+		if(list.isEmpty()) {
+			return null;
+		}else {
+			return (Product)list.get(0);
+		}
+	}
+
+
+	public int productFileInsert(ProductFile productList) {
+		String query = "insert into product_file values(product_file_seq.nextval,?,?,?)";
+		Object[] params = {productList.getFilename(),productList.getFilepath(),productList.getProductNo()};
+		int result = jdbc.update(query, params);
+		return result;
+	}
 }
