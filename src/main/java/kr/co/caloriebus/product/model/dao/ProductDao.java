db46069df20bb08c424677c4e89f540ae3f83daf
seong3449 <tpdud3449@naver.com>
2024-08-07 10:20:43 +0900

    조건 변경 커밋

--- src/main/java/kr/co/caloriebus/product/model/dao/ProductDao.java
+++ src/main/java/kr/co/caloriebus/product/model/dao/ProductDao.java
@@ -21,7 +21,7 @@ public class ProductDao {
 	private ProductFileRowMapper productFileRowMapper;
 	
 	public List selectAllProduct() {
-		String query="select * from product order by 1 desc";
+		String query="select * from product";
 		List list = jdbc.query(query, productRowMapper);
 		return list;
 	}
