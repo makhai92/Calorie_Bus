b9a42b4f26f083c2fe10ccd1784b9b3e23f59f2a
rlawl135 <rlawl135@gmail.com>
2024-08-07 14:21:44 +0900

    수정1

--- src/main/java/kr/co/caloriebus/exercise/model/dao/ExerciseDao.java
+++ src/main/java/kr/co/caloriebus/exercise/model/dao/ExerciseDao.java
@@ -22,92 +22,102 @@ public class ExerciseDao {
 	private ExerciseFileRowMapper exerciseFileRowMapper;
 	@Autowired
 	private ExerciseCommentRowMapper exerciseCommentRowMapper;
-
-	public List selectBoardList(String category, int start, int end) {
-		String query = "select * from (select rownum rnum, b.*\r\n" + 
-				"from ((select board_no,member_no,board_title,board_category,read_count,reg_date,member_id as board_writer,(select count(*) from board_comment where board_ref=board.board_no) as comment_count,(select count(*) from board_like where board_no=board.board_no) as like_count from board join member using(member_no) where board_category=? order by 1 desc)b)) where rnum between ? and ?";
-		Object[] params = {category,start,end};
-		List list = jdbc.query(query, exerciseRowMapper,params);
-		return list;
-	}
 	
-	public List selectBoardList(int start,int end) {
+	public List selectBoardList(int start, int end) {
 		String query = "select * from (select rownum rnum, b.*\r\n" + 
-				"from ((select board_no,member_no,board_title,board_category,read_count,reg_date,member_id as board_writer,(select count(*) from board_comment where board_ref=board.board_no) as comment_count,(select count(*) from board_like where board_no=board.board_no) as like_count from board join member using(member_no) where board_category in ('I1') order by 1 desc)b)) where rnum between ? and ?";
+				"from ((select board_no,member_no,board_title,board_category,read_count,reg_date,member_id as board_writer,(select count(*) from board_comment where board_ref=board.board_no) as comment_count,(select count(*) from board_like where board_no=board.board_no) as like_count from board join member using(member_no) where board_category='I1' order by 1 desc)b))bb where rnum between ? and ?";
 		Object[] params = {start,end};
-		List list = jdbc.query(query, exerciseRowMapper,params);
+		List list = jdbc.query(query,exerciseRowMapper, params);
 		return list;
 	}
 
-	public int selectBoardTotalCount(String category) {
-		String query = "select count(*) from board where board_category=?";
-		Object[] params = {category};
-		int totalCount = jdbc.queryForObject(query, Integer.class, params);
-		return totalCount;
-	}
-	
 	public int selectBoardTotalCount() {
-		String query = "select count(*) from board where board_category in ?";
-		Object[] params = {"I1"};
-		int totalCount = jdbc.queryForObject(query, Integer.class, params);
+		String query = "select count(*) from board_category='I1'";
+		int totalCount = jdbc.queryForObject(query, Integer.class);
 		return totalCount;
 	}
+
 	public int insertBoard(Exercise e) {
-		String query = "insert into board values(board_seq.nextval,?,'I1',?,?,1,to_char(sysdate,'YYYY-MM-DD'))";
-		Object[] params = {e.getMemberNo(),e.getBoardTitle(),e.getBoardContent()};
-		int result = jdbc.update(query, params);
+		String query = "insert into board values(board_seq.nextval,?,I1,?,?,1,to_char(sysdate,'yyyy-mm-dd'))";
+		Object[] params = {e.getBoardTitle(),e.getBoardWriter(),e.getBoardContent()};
+		int result = jdbc.update(query,params);
 		return result;
 	}
+
 	public int selectBoardNo() {
 		String query = "select max(board_no) from board";
 		int boardNo = jdbc.queryForObject(query, Integer.class);
 		return boardNo;
 	}
+
 	public int insertBoardFile(ExerciseFile exerciseFile) {
 		String query = "insert into board_file values(board_file_seq.nextval,?,?,?)";
-		Object[] params = {exerciseFile.getFilename(),exerciseFile.getFilepath(),exerciseFile.getBoardNo()};
+		Object[] params = {exerciseFile.getBoardNo(),exerciseFile.getFilename(),exerciseFile.getFilepath()};
 		int result = jdbc.update(query,params);
 		return result;
 	}
-	//운동정보
+
 	public Exercise selectOneBoard(int boardNo) {
 		String query = "select * from board where board_no=?";
 		Object[] params = {boardNo};
 		List list = jdbc.query(query, exerciseRowMapper, params);
 		if(list.isEmpty()) {
-			return null;			
+			return null;
 		}
 		return (Exercise)list.get(0);
 	}
+
 	public int updateReadCount(int boardNo) {
 		String query = "update board set read_count = read_count+1 where board_no=?";
 		Object[] params = {boardNo};
 		int result = jdbc.update(query,params);
 		return result;
 	}
-	public List selectBoardfile(int boardNo) {
-		String query = "select * from board_file where board_no=?";
-		Object[] params = {boardNo};
-		List list = jdbc.query(query, exerciseFileRowMapper, params);
-		return list;
-	}
-	public List selectReCommentList(int boardNo, int memberNo) {
+
+	public List selectBoardFile(int boardNo) {
 		String query = "select * from board_file where board_no=?";
 		Object[] params = {boardNo};
 		List list = jdbc.query(query, exerciseFileRowMapper,params);
 		return list;
 	}
-	/*
-	public List selectCommentList(int boardNo, int memberNo) {
-		String query =  "select\r\n" + 
-				"   nc.*,\r\n" + 
-				"    (select count(*) from board_comment_like where board_comment_no=ec.board_comment_no) as like_count,\r\n" + 
-				"    (select count(*) from board_comment_like where board_comment_no=ec.board_comment_no and member_no=?) as is_like\r\n" + 
-				"from board_comment nc\r\n" + 
-				"where board_ref=? and board_comment_ref is null order by 1";
-		return null;
+
+	public int deleteBoard(int boardNo) {
+		String query = "delete from board_file where file_no=?";
+		Object[] params = {boardNo};
+		int result = jdbc.update(query,params);
+		return result;
 	}
-	 */
+
+	public int updateBoard(Exercise e) {
+		String query = "update board set board_title=?,board_content=?,read_count = read_count-1 where board_no=?";
+		Object[] params = {e.getBoardTitle(),e.getBoardContent(),e.getReadCount(),e.getBoardNo()};
+		int result = jdbc.update(query,params);
+		return result;
+	}
+
+	public ExerciseFile selectOneBoardfile(int fileNo) {
+		String query = "select * from board_file where file_no=?";
+		Object[] params = {fileNo};
+		List list = jdbc.query(query, exerciseFileRowMapper, params);
+		if(list.isEmpty()) {
+			return null;			
+		}
+		return (ExerciseFile)list.get(0);
+	}
+
+	public int deleteBoardFile(int fileNo) {
+		String query = "delete from board_file where file_no=?";
+		Object[] params = {fileNo};
+		int result = jdbc.update(query,params);
+		return result;
+	}
+	
+	
+	
+	
+	
+
+	
 	
 	
 }
