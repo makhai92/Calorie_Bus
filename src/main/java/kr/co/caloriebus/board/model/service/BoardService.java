c20cc8b9830ba16e932696a853c4bb02f981239b
오시우 <skysiwoo5@naver.com>
2024-08-09 17:51:57 +0900

    게시판가짜끝

--- src/main/java/kr/co/caloriebus/board/model/service/BoardService.java
+++ src/main/java/kr/co/caloriebus/board/model/service/BoardService.java
@@ -137,6 +137,102 @@ public class BoardService {
 		}
 	}
 
+	public BoardListData searchBoardList(String category, String type, String keyword, int reqPage) {
+		int numPerPage = 10;
+		int end = reqPage*numPerPage;
+		int start = end-numPerPage+1;
+		List list = null;
+		int totalCount = 0;
+		if(category.equals("all")) {
+			if(type.equals("title")) {
+				list = boardDao.selectBoardTitleList(start,end,keyword);
+				totalCount = boardDao.selectBoardTitleTotalCount(keyword);
+			}else if(type.equals("writer")) {
+				list = boardDao.selectBoardWriterList(start,end,keyword);
+				totalCount = boardDao.selectBoardWriterTotalCount(keyword);
+			}
+		}else {
+			if(type.equals("title")) {
+				list = boardDao.selectBoardTitleList(category,start,end,keyword);
+				totalCount = boardDao.selectBoardTotalTitleCount(category,keyword);
+			}else if(type.equals("writer")) {
+				list = boardDao.selectBoardWriterList(category,start,end,keyword);
+				totalCount = boardDao.selectBoardTotalWriterCount(category,keyword);
+			}
+		}
+		int totalPage = 0;
+		if(totalCount%numPerPage == 0) {
+			totalPage = totalCount/numPerPage;
+		}else {
+			totalPage = totalCount/numPerPage+1;
+		}
+		int pageNaviSize = 5;
+		int pageNo = ((reqPage-1)/pageNaviSize)*pageNaviSize+1;
+		String pageNavi = "<ul class='page-ul'>";
+		if(pageNo != 1) {
+			pageNavi += "<li>";
+			pageNavi += "<a class='page-item' href='/board/list?category="+category+"&reqPage="+(pageNo-1)+"&type="+type+"&keyword="+keyword+"'>";
+			pageNavi += "<span class='material-icons'>chevron_left</span>";
+			pageNavi += "</a></li>";
+		}
+		for(int i=0;i<pageNaviSize;i++) {
+			pageNavi += "<li>";
+			if(pageNo == reqPage) {
+				pageNavi += "<a class='page-item active' href='/board/list?category="+category+"&reqPage="+pageNo+"&type="+type+"&keyword="+keyword+"'>";
+			}else {
+				pageNavi += "<a class='page-item' href='/board/list?category="+category+"&reqPage="+pageNo+"&type="+type+"&keyword="+keyword+"'>";
+			}
+			pageNavi += pageNo;
+			pageNavi += "</a></li>";
+			pageNo++;
+			if(pageNo > totalPage) {
+				break;
+			}
+		}
+		if(pageNo <= totalPage) {
+			pageNavi += "<li>";
+			pageNavi += "<a class='page-item' href='/board/list?category="+category+"&reqPage="+pageNo+"&type="+type+"&keyword="+keyword+"'>";
+			pageNavi += "<span class='material-icons'>chevron_right</span>";
+			pageNavi += "</a></li>";
+		}
+		pageNavi += "</ul>";
+		BoardListData bld = new BoardListData(list, pageNavi);
+		return bld;
+	}
+	public List boardReCommentList(int boardCommentNo,int memberNo) {
+		List list = boardDao.selectBoardReCommentList(boardCommentNo, memberNo);
+		return list;
+	}
+
+	@Transactional
+	public int updateComment(BoardComment bc) {
+		int result = boardDao.updateComment(bc);
+		return result;
+	}
+
+	@Transactional
+	public int deleteComment(int boardCommentNo) {
+		int result = boardDao.deleteComment(boardCommentNo);
+		return result;
+	}
+
+	@Transactional
+	public List<BoardFile> deleteBoard(int boardNo) {
+		List list = boardDao.selectBoardFileList(boardNo);
+		int result = boardDao.deleteBoard(boardNo);
+		if(result > 0) {
+			return list;
+		}
+		return null;
+	}
+
+	public Board selectOneBoard(int boardNo) {
+		Board board = boardDao.selectOneBoard(boardNo);
+		List list = boardDao.selectBoardFileList(boardNo);
+		board.setFileList(list);
+		return board;
+	}
+	
 	// 마이페이지 용 내 게시글 보기
 	public BoardListData selectMyBoardList(int memberNo, int reqPage) {
 		int numPerPage = 10;
@@ -188,20 +284,32 @@ public class BoardService {
 		return bld;
 	}
 
-	public List boardReCommentList(int boardCommentNo,int memberNo) {
-		List list = boardDao.selectBoardReCommentList(boardCommentNo, memberNo);
-		return list;
+	@Transactional
+	public List<BoardFile> updateBoard(Board b, ArrayList<BoardFile> fileList,int[] delFileNo) {
+		List<BoardFile> delFileList = new ArrayList<BoardFile>();
+		int result = boardDao.updateBoard(b);
+		if(result > 0) {
+			for(BoardFile file : fileList) {
+				file.setBoardNo(b.getBoardNo());
+				result += boardDao.insertBoardFile(file);
+			}
+			if(delFileNo != null) {
+				for(int fileNo : delFileNo) {
+					BoardFile boardFile = boardDao.selectOneBoardFile(fileNo);
+					result += boardDao.deleteBoardFile(fileNo);
+					delFileList.add(boardFile);
+				}
+			}
+		}
+		int resultTotal = delFileNo == null?fileList.size()+1:fileList.size()+1+delFileNo.length;
+		if(resultTotal == result) {
+			return delFileList;
+		}else {
+			return null;
+		}
+		
 	}
 
-	@Transactional
-	public int updateComment(BoardComment bc) {
-		int result = boardDao.updateComment(bc);
-		return result;
-	}
+	
 
-	@Transactional
-	public int deleteComment(int boardCommentNo) {
-		int result = boardDao.deleteComment(boardCommentNo);
-		return result;
-	}
 }
